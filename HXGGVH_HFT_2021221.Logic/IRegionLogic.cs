6cdf57637b3371799a005fee0378f6dbc9ac9e4d
zolnaiL1 <LaszloZolnai@stud.uni-obuda.hu>
2021-11-16 12:08:26 +0100

    Logic interfaces filled up with CRUD methods

--- HXGGVH_HFT_2021221.Logic/IRegionLogic.cs
+++ HXGGVH_HFT_2021221.Logic/IRegionLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HXGGVH_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace HXGGVH_HFT_2021221.Logic
 {
     public interface IRegionLogic
     {
+        public void Create(Region region);
+        Region Read(int id);
+        IQueryable<Region> ReadAll();
+        public void Delete(int id);
+        public void Update(Region region);
     }
 }
--- HXGGVH_HFT_2021221.Logic/ITrainerLogic.cs
+++ HXGGVH_HFT_2021221.Logic/ITrainerLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HXGGVH_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace HXGGVH_HFT_2021221.Logic
 {
     public interface ITrainerLogic
     {
+        public void Create(Trainer trainer);
+        public void Delete(int id);
+        Trainer Read(int id);
+        IQueryable<Trainer> ReadAll();
+        public void Update(Trainer trainer);
     }
 }
