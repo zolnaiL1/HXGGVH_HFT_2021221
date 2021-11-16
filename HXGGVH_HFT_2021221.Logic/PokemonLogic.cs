ffba3a240c01be70afeed5ad305e970fa7715e49
zolnaiL1 <LaszloZolnai@stud.uni-obuda.hu>
2021-11-16 13:32:30 +0100

    PokemonLogic filled up with CRUD methods

--- HXGGVH_HFT_2021221.Logic/PokemonLogic.cs
+++ HXGGVH_HFT_2021221.Logic/PokemonLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HXGGVH_HFT_2021221.Models;
+using HXGGVH_HFT_2021221.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,45 @@ using System.Threading.Tasks;
 
 namespace HXGGVH_HFT_2021221.Logic
 {
-    public class PokemonLogic
+    public class PokemonLogic : IPokemonLogic
     {
+        IPokemonRepository pokemonRepo;
+        public PokemonLogic(IPokemonRepository pokemonRepo)
+        {
+            this.pokemonRepo = pokemonRepo;
+        }
+
+        //CRUD: Create, Read, ReadAll, Update, Delete
+        public void Create(Pokemon pokemon)
+        {
+            if (pokemon.Name == null)
+            {
+                throw new ArgumentException("Name is null!");
+            }
+            pokemonRepo.Create(pokemon);
+        }
+
+        public Pokemon Read(int id)
+        {
+            return pokemonRepo.Read(id);
+        }
+
+        public IQueryable<Pokemon> ReadAll()
+        {
+            return pokemonRepo.ReadAll();
+        }
+
+        public void Delete(int id)
+        {
+            pokemonRepo.Delete(id);
+        }
+
+        public void Update(Pokemon pokemon)
+        {
+            pokemonRepo.Update(pokemon);
+        }
+
+        //NON-CRUD
+        //1
     }
 }
