97e89d6fa350a6ffe562b2835b6792600b70f513
zolnaiL1 <LaszloZolnai@stud.uni-obuda.hu>
2021-11-16 13:39:46 +0100

    RegionLogic filled up with CRUD methods

--- HXGGVH_HFT_2021221.Logic/RegionLogic.cs
+++ HXGGVH_HFT_2021221.Logic/RegionLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HXGGVH_HFT_2021221.Models;
+using HXGGVH_HFT_2021221.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,42 @@ using System.Threading.Tasks;
 
 namespace HXGGVH_HFT_2021221.Logic
 {
-    public class RegionLogic
+    public class RegionLogic : IRegionLogic
     {
+        IRegionRepository regionRepo;
+        public RegionLogic(IRegionRepository regionRepo)
+        {
+            this.regionRepo = regionRepo;
+        }
+
+        //CRUD: Create, Read, ReadAll, Update, Delete
+        public void Create(Region region)
+        {
+            if (region.Name == null)
+            {
+                throw new ArgumentException("Name is null!");
+            }
+            regionRepo.Create(region);
+        }
+
+        public Region Read(int id)
+        {
+            return regionRepo.Read(id);
+        }
+
+        public IQueryable<Region> ReadAll()
+        {
+            return regionRepo.ReadAll();
+        }
+
+        public void Delete(int id)
+        {
+            regionRepo.Delete(id);
+        }
+
+        public void Update(Region region)
+        {
+            regionRepo.Update(region);
+        }
     }
 }
