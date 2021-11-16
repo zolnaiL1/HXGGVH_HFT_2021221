9dfca68732bf191f4fbdb00ca99967831f18f56a
zolnaiL1 <LaszloZolnai@stud.uni-obuda.hu>
2021-11-16 13:44:20 +0100

    TrainerLogic filled up with CRUD methods

--- HXGGVH_HFT_2021221.Logic/TrainerLogic.cs
+++ HXGGVH_HFT_2021221.Logic/TrainerLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HXGGVH_HFT_2021221.Models;
+using HXGGVH_HFT_2021221.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,42 @@ using System.Threading.Tasks;
 
 namespace HXGGVH_HFT_2021221.Logic
 {
-    public class TrainerLogic
+    public class TrainerLogic : ITrainerLogic
     {
+        ITrainerRepository trainerRepo;
+        public TrainerLogic(ITrainerRepository trainerRepo)
+        {
+            this.trainerRepo = trainerRepo;
+        }
+
+        //CRUD: Create, Read, ReadAll, Update, Delete
+        public void Create(Trainer trainer)
+        {
+            if (trainer.Name == null)
+            {
+                throw new ArgumentException("Name is null!");
+            }
+            trainerRepo.Create(trainer);
+        }
+
+        public Trainer Read(int id)
+        {
+            return trainerRepo.Read(id);
+        }
+
+        public IQueryable<Trainer> ReadAll()
+        {
+            return trainerRepo.ReadAll();
+        }
+
+        public void Delete(int id)
+        {
+            trainerRepo.Delete(id);
+        }
+
+        public void Update(Trainer trainer)
+        {
+            trainerRepo.Update(trainer);
+        }
     }
 }
