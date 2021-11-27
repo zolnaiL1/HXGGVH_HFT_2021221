89856a172354311f571503b3497ba87b2154db66
zolnaiL1 <LaszloZolnai@stud.uni-obuda.hu>
2021-11-27 14:08:25 +0100

    TrainerController class created

---
+++ HXGGVH_HFT_2021221.Endpoint/Controllers/TrainerController.cs
@@ -0,0 +1,59 @@
+﻿using HXGGVH_HFT_2021221.Logic;
+using HXGGVH_HFT_2021221.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HXGGVH_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TrainerController : ControllerBase
+    {
+        ITrainerLogic trainerLogic;
+
+        public TrainerController(ITrainerLogic trainerLogic)
+        {
+            this.trainerLogic = trainerLogic;
+        }
+
+        // GETALL
+        [HttpGet]
+        public IEnumerable<Trainer> Get()
+        {
+            return trainerLogic.ReadAll();
+        }
+
+        // GET 
+        [HttpGet("{id}")]
+        public Trainer Get(int id)
+        {
+            return trainerLogic.Read(id);
+        }
+
+        // POST 
+        [HttpPost]
+        public void Post([FromBody] Trainer value)
+        {
+            trainerLogic.Create(value);
+        }
+
+        // PUT 
+        [HttpPut]
+        public void Put([FromBody] Trainer value)
+        {
+            trainerLogic.Update(value);
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            trainerLogic.Delete(id);
+        }
+    }
+}
--- HXGGVH_HFT_2021221.Models/Trainer.cs
+++ HXGGVH_HFT_2021221.Models/Trainer.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace HXGGVH_HFT_2021221.Models
@@ -27,6 +28,7 @@ namespace HXGGVH_HFT_2021221.Models
         public int RegionID { get; set; }
 
         [NotMapped]
+        [JsonIgnore]
         public virtual Region Region { get; set; }
         [NotMapped]
         public virtual ICollection<Pokemon> Pokemons { get; set; }
